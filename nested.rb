8c6d43ebe7dfc155920b44aa4be0a04136696fd9
Rafael Cruz <ralph1786@gmail.com>
2018-11-24 22:24:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie]
+     language = programmer_hash[:dennis_ritchie][:languages]
+     language[0]
 end
 
 def adding_matz
