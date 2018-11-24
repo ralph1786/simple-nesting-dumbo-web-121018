24ee0b9cd650cc58e1b5ebb43c7c83f5aeefc101
Rafael Cruz <ralph1786@gmail.com>
2018-11-24 22:30:25 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     language = programmer_hash[:dennis_ritchie][:languages]
+     language[0]
 end
 
 def adding_matz
@@ -79,8 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    programmer_hash
 end
 
 def changing_alan
@@ -99,10 +102,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  language = programmer_hash[:dennis_ritchie][:languages]
+  language[1] = "Assembly"
+  programmer_hash
 end
