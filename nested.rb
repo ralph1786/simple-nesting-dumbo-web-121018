1d6dd1027bf2e16dce4f063a0aba39346c434b52
Rafael Cruz <ralph1786@gmail.com>
2018-11-24 22:30:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  language = programmer_hash[:dennis_ritchie][:languages]
+  language[1] = "Assembly"
+  programmer_hash
 end
